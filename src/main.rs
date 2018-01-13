395f812553d9ac4fbc5806a4fe067b74c60fdbe2
Antoni Boucher <bouanto@zoho.com>
2018-01-13 12:10:03 -0500

    Add many commands

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 #[macro_use]
 extern crate may;
+extern crate time;
 
-use std::str;
-use std::path::PathBuf;
+mod cmd;
+mod error;
+
+use std::env;
+use std::fs::{File, Metadata, create_dir, read_dir, remove_dir_all};
 use std::io::{self, Read, Write};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::path::{Path, PathBuf, StripPrefixError};
+use std::result;
+use std::str;
 
 use may::net::{TcpListener, TcpStream};
+
+use cmd::{Command, TransferType};
+
+const CONFIG_FILE: &'static str = "config.toml";
+const MONTHS: [&'static str; 12] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                                    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u32)]
@@ -52,87 +66,213 @@ enum ResultCode {
     FileNameNotAllowed = 553,
 }
 
-#[derive(Clone, Debug)]
-enum Command {
-    Auth,
-    Syst,
-    Unknown(String),
-    User(String),
-}
-
-impl AsRef<str> for Command {
-    fn as_ref(&self) -> &str {
-        match *self {
-            Command::Auth => "AUTH",
-            Command::Syst => "SYST",
-            Command::Unknown(_) => "UNKN",
-            Command::User(_) => "USER",
-        }
-    }
-}
-
-impl Command {
-    pub fn new(input: Vec<u8>) -> io::Result<Self> {
-        let mut iter = input.split(|&byte| byte == b' ');
-        let mut command = iter.next().expect("command in input").to_vec();
-        to_uppercase(&mut command);
-        let data = iter.next();
-        let command =
-            match command.as_slice() {
-                b"AUTH" => Command::Auth,
-                b"SYST" => Command::Syst,
-                b"USER" => Command::User(data.map(|bytes| String::from_utf8(bytes.to_vec())
-                                                                 .expect("cannot convert bytes to String"))
-                                             .unwrap_or_default()),
-                s => Command::Unknown(str::from_utf8(s).unwrap_or("").to_owned()),
-            };
-        Ok(command)
-    }
-}
-
-fn to_uppercase(data: &mut [u8]) {
-    for byte in data {
-        if *byte >= 'a' as u8 && *byte <= 'z' as u8 {
-            *byte -= 32;
-        }
-    }
-}
-
 #[allow(dead_code)]
 struct Client {
     cwd: PathBuf,
-    stream: TcpStream,
+    data_port: Option<u16>,
+    data_stream: Option<TcpStream>,
+    has_quit: bool,
+    is_admin: bool,
     name: Option<String>,
+    server_root: PathBuf,
+    stream: TcpStream,
+    transfer_type: TransferType,
 }
 
 impl Client {
-    fn new(stream: TcpStream) -> Client {
+    fn new(stream: TcpStream, server_root: PathBuf) -> Client {
         Client {
             cwd: PathBuf::from("/"),
-            stream: stream,
+            data_port: None,
+            data_stream: None,
+            has_quit: false,
+            is_admin: false,
             name: None,
+            server_root,
+            stream,
+            transfer_type: TransferType::Ascii,
         }
     }
 
-    fn handle_cmd(&mut self, cmd: Command) {
+    fn handle_cmd(&mut self, cmd: Command) -> io::Result<()> {
         println!("====> {:?}", cmd);
         match cmd {
-            Command::Auth => send_cmd(&mut self.stream, ResultCode::CommandNotImplemented,
-                                      "Not implemented"),
-            Command::Syst => send_cmd(&mut self.stream, ResultCode::Ok, "I won't tell"),
-            Command::User(username) => {
-                if username.is_empty() {
-                    send_cmd(&mut self.stream, ResultCode::InvalidParameterOrArgument,
-                             "Invalid username")
+            Command::Auth => self.send(ResultCode::CommandNotImplemented, "Not implemented"),
+            Command::Cwd(directory) => self.cwd(directory),
+            Command::List(path) => self.list(path),
+            Command::NoOp => self.send(ResultCode::Ok, "Doing nothing"),
+            Command::Pasv => self.pasv(),
+            Command::Port(port) => {
+                self.data_port = Some(port);
+                self.send(ResultCode::Ok, &format!("Data port is now {}", port))
+            },
+            Command::Pwd => {
+                let msg = format!("{}", self.cwd.to_str().unwrap_or("")); // small trick
+                if !msg.is_empty() {
+                    let message = format!("\"{}\" ", msg);
+                    self.send(ResultCode::PATHNAMECreated, &message)
                 } else {
-                    self.name = Some(username.to_owned());
-                    send_cmd(&mut self.stream, ResultCode::UserLoggedIn,
-                             &format!("Welcome {}!", username))
+                    self.send(ResultCode::FileNotFound, "No such file or directory")
                 }
             }
-            Command::Unknown(s) => send_cmd(&mut self.stream, ResultCode::UnknownCommand,
-                                            &format!("Not implemented: '{}'", s)),
+            Command::Quit => self.quit(),
+            Command::Syst => self.send(ResultCode::Ok, "I won't tell"),
+            Command::Type(typ) => {
+                self.transfer_type = typ;
+                self.send(ResultCode::Ok, "Transfer type changed successfully")
+            },
+            Command::User(username) => {
+                if username.is_empty() {
+                    self.send(ResultCode::InvalidParameterOrArgument, "Invalid username")
+                } else {
+                    self.name = Some(username.to_owned());
+                    self.send(ResultCode::UserLoggedIn, &format!("Welcome {}!", username))
+                }
+            },
+            s => self.send(ResultCode::UnknownCommand, &format!("Not implemented: '{:?}'", s)),
         }
+    }
+
+    fn list(&mut self, path: Option<PathBuf>) -> io::Result<()> {
+        if self.data_stream.is_some() {
+            let path = self.cwd.join(path.unwrap_or_default());
+            let directory = PathBuf::from(&path);
+            let res = self.complete_path(directory);
+            if let Ok(path) = res {
+                self.send(ResultCode::DataConnectionAlreadyOpen, "Starting to list directory...")?;
+                let mut out = vec![];
+                if path.is_dir() {
+                    if let Ok(dir) = read_dir(path) {
+                        for entry in dir {
+                            if let Ok(entry) = entry {
+                                if self.is_admin ||
+                                   entry.path() != self.server_root.join(CONFIG_FILE) {
+                                    add_file_info(entry.path(), &mut out);
+                                }
+                            }
+                        }
+                    } else {
+                        self.send(ResultCode::InvalidParameterOrArgument, "No such file or directory")?;
+                        return Ok(());
+                    }
+                } else if self.is_admin || path != self.server_root.join(CONFIG_FILE) {
+                    add_file_info(path, &mut out);
+                }
+                self.send_data(out)?;
+                println!("-> and done!");
+            } else {
+                self.send(ResultCode::InvalidParameterOrArgument, "No such file or directory")?;
+            }
+        } else {
+            self.send(ResultCode::ConnectionClosed, "No opened data connection")?;
+        }
+        if self.data_stream.is_some() {
+            self.close_data_connection();
+            self.send(ResultCode::ClosingDataConnection, "Transfer done")?;
+        }
+        Ok(())
+    }
+
+    fn close_data_connection(&mut self) {
+        self.data_stream = None;
+    }
+
+    fn pasv(&mut self) -> io::Result<()> {
+        let port =
+            if let Some(port) = self.data_port {
+                port
+            } else {
+                0
+            };
+        if self.data_stream.is_some() {
+            return self.send(ResultCode::DataConnectionAlreadyOpen, "Already listening...");
+        }
+
+        let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+        let listener = TcpListener::bind(&addr)?;
+        let port = listener.local_addr()?.port();
+
+        self.send(ResultCode::EnteringPassiveMode, &format!("127,0,0,1,{},{}", port >> 8, port & 0xFF))?;
+
+        println!("Waiting clients on port {}...", port);
+        match listener.incoming().next() {
+            Some(stream) => self.data_stream = Some(stream?),
+            None => unreachable!(),
+        }
+
+        Ok(())
+    }
+
+    fn cwd(&mut self, directory: PathBuf) -> io::Result<()> {
+        let path = self.cwd.join(&directory);
+        let res = self.complete_path(path);
+        if let Ok(dir) = res {
+            let res = self.strip_prefix(dir);
+            if let Ok(prefix) = res {
+                self.cwd = prefix.to_path_buf();
+                prefix_slash(&mut self.cwd);
+                self.send(ResultCode::RequestedFileActionOkay, &format!("Directory changed to \"{}\"",
+                                                             directory.display()))?;
+            }
+        }
+        self.send(ResultCode::FileNotFound, "No such file or directory")
+    }
+
+    fn complete_path(&self, path: PathBuf) -> io::Result<PathBuf> {
+        let directory = self.server_root.join(if path.has_root() {
+            path.iter().skip(1).collect()
+        } else {
+            path
+        });
+        let dir = directory.canonicalize();
+        if let Ok(ref dir) = dir {
+            if !dir.starts_with(&self.server_root) {
+                return Err(io::ErrorKind::PermissionDenied.into());
+            }
+        }
+        dir
+    }
+
+    fn strip_prefix(&self, dir: PathBuf) -> result::Result<PathBuf, StripPrefixError> {
+        dir.strip_prefix(&self.server_root).map(|p| p.to_path_buf())
+    }
+
+    fn send(&mut self, result: ResultCode, msg: &str) -> io::Result<()> {
+        send_cmd(&mut self.stream, result, msg)
+    }
+
+    fn send_data(&mut self, data: Vec<u8>) -> io::Result<()> {
+        if let Some(ref mut stream) = self.data_stream {
+            stream.write_all(&data)?;
+        }
+        Ok(())
+    }
+
+    fn run(&mut self) -> io::Result<()> {
+        while !self.has_quit {
+            let data = read_all_message(&mut self.stream);
+            if data.is_empty() {
+                println!("client disconnected...");
+                break;
+            }
+            if let Ok(command) = Command::new(data) {
+                self.handle_cmd(command)?;
+            } else {
+                println!("Error with client command...");
+            }
+        }
+        Ok(())
+    }
+
+    fn quit(&mut self) -> io::Result<()> {
+        // TODO
+        if self.data_stream.is_some() {
+            unimplemented!();
+        } else {
+            self.send(ResultCode::ServiceClosingControlConnection, "Closing connection...")?;
+            self.has_quit = true;
+        }
+        Ok(())
     }
 }
 
@@ -159,43 +299,36 @@ fn read_all_message(stream: &mut TcpStream) -> Vec<u8> {
     }
 }
 
-fn send_cmd(stream: &mut TcpStream, code: ResultCode, message: &str) {
+fn send_cmd(stream: &mut TcpStream, code: ResultCode, message: &str) -> io::Result<()> {
     let msg = if message.is_empty() {
         format!("{}\r\n", code as u32)
     } else {
         format!("{} {}\r\n", code as u32, message)
     };
     println!("<==== {}", msg);
-    write!(stream, "{}", msg).unwrap()
+    write!(stream, "{}", msg)
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, server_root: PathBuf) -> io::Result<()> {
     println!("new client connected!");
-    send_cmd(&mut stream, ResultCode::ServiceReadyForNewUser, "Welcome to this FTP server!");
-    let mut client = Client::new(stream);
-    loop {
-        let data = read_all_message(&mut client.stream);
-        if data.is_empty() {
-            println!("client disconnected...");
-            break;
-        }
-        if let Ok(command) = Command::new(data) {
-            client.handle_cmd(command);
-        } else {
-            println!("Error with client command...");
-        }
-    }
+    send_cmd(&mut stream, ResultCode::ServiceReadyForNewUser, "Welcome to this FTP server!")?;
+    let mut client = Client::new(stream, server_root);
+    client.run()
 }
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:1234").expect("Couldn't bind this address...");
+fn server() -> io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:1234")?;
+    let server_root = env::current_dir()?;
 
     println!("Waiting for clients to connect...");
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
+                let server_root = server_root.clone();
                 go!(|| {
-                    handle_client(stream);
+                    if let Err(error) = handle_client(stream, server_root) {
+                        println!("Error handling client: {}", error)
+                    }
                 });
             }
             _ => {
@@ -203,4 +336,70 @@ fn main() {
             }
         }
     }
+    Ok(())
+}
+
+fn main() {
+    if let Err(error) = server() {
+        println!("Error running the server: {}", error);
+    }
+}
+
+fn prefix_slash(path: &mut PathBuf) {
+    if !path.is_absolute() {
+        *path = Path::new("/").join(&path);
+    }
+}
+
+// If an error occurs when we try to get file's information, we just return and don't send its info.
+fn add_file_info(path: PathBuf, out: &mut Vec<u8>) {
+    let extra = if path.is_dir() { "/" } else { "" };
+    let is_dir = if path.is_dir() { "d" } else { "-" };
+
+    let meta = match ::std::fs::metadata(&path) {
+        Ok(meta) => meta,
+        _ => return,
+    };
+    let (time, file_size) = get_file_info(&meta);
+    let path = match path.to_str() {
+        Some(path) => match path.split("/").last() {
+            Some(path) => path,
+            _ => return,
+        },
+        _ => return,
+    };
+    // TODO: maybe improve how we get rights in here?
+    let rights = if meta.permissions().readonly() {
+        "r--r--r--"
+    } else {
+        "rw-rw-rw-"
+    };
+    let file_str = format!("{is_dir}{rights} {links} {owner} {group} {size} {month} {day} {hour}:{min} {path}{extra}\r\n",
+                           is_dir=is_dir,
+                           rights=rights,
+                           links=1, // number of links
+                           owner="anonymous", // owner name
+                           group="anonymous", // group name
+                           size=file_size,
+                           month=MONTHS[time.tm_mon as usize],
+                           day=time.tm_mday,
+                           hour=time.tm_hour,
+                           min=time.tm_min,
+                           path=path,
+                           extra=extra);
+    out.extend(file_str.as_bytes());
+    println!("==> {:?}", &file_str);
+}
+
+#[cfg(windows)]
+fn get_file_info(meta: &Metadata) -> (time::Tm, u64) {
+    use std::os::windows::prelude::*;
+    (time::at(time::Timespec::new((meta.last_write_time() / 10_000_000) as i64, 0)),
+    meta.file_size())
+}
+
+#[cfg(not(windows))]
+fn get_file_info(meta: &Metadata) -> (time::Tm, u64) {
+    use std::os::unix::prelude::*;
+    (time::at(time::Timespec::new(meta.mtime(), 0)), meta.size())
 }
